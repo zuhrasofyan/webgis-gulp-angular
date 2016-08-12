758dcb118ce74ae944a7404298d67d160b8a2f61
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-08-12 10:59:23 +0700

    Add font-awesome asset for gulp build

--- gulp/build.js
+++ gulp/build.js
@@ -50,11 +50,13 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(jsFilter.restore)
     .pipe(cssFilter)
     // .pipe($.sourcemaps.init())
-    .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
+    
     .pipe($.cssnano())
     .pipe($.rev())
     // .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
+    .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
+    .pipe($.replace('../../bower_components/font-awesome/fonts', '../fonts'))
     .pipe($.revReplace())
     .pipe(htmlFilter)
     .pipe($.htmlmin({
