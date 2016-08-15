977375bfa470775a5160e2f9d05f660d83451f83
zuhrasofyan <zuhrasofyan@gmail.com>
2016-08-15 14:02:48 +0700

    add jalan and lokasi basemap layers from geoserver instead openmap URL

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,19 +6,50 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController($scope, $http, olData) {
     var vm = this;
 
-    vm.london = {
-            lat: 51.505,
-            lon: -0.09,
-            zoom: 4
-        };
     vm.bandaAceh = {
         lat: 5.551,
         lon: 95.322,
-        zoom: 13
+        zoom: 15,
+        minZoom: 14,
+        maxZoom: 20
     };
+    vm.defaults = {
+      view: {
+          maxZoom: 20,
+          minZoom: 14
+      }
+    }
+    vm.wms = {
+      source: {
+        type: 'ImageWMS',
+        url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+        params:{'LAYERS': 'uptb_gis_bna:jalan_line_wgs0', 'TILED': true}
+      }
+    };
+
+    vm.layers = [
+      {
+        name: 'jalan',
+        active: true,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:jalan_line_wgs0', 'TILED': true}
+        }
+      }, 
+      {
+        name: 'lokasi',
+        active: true,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:lokasi_tabel', 'TILED': true}
+        }
+      }];
+
   }
   
 })();
