8b6a79a93d4dc65acca5b627512cf6f94ef0a46b
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-08-16 16:41:57 +0700

    Add tematik and perencanaan layers
    also, test toggle on/off basic map

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,8 @@
   function MainController($scope, $http, olData) {
     var vm = this;
 
+    vm.tampil = true;
+
     //set base coordinate
     vm.bandaAceh = {
         lat: 5.551,
@@ -22,18 +24,23 @@
           minZoom: 14,
           extent: [10605714.11, 615365.85, 10617256.10, 625990.60]
       }
-    }
+    };
 
-
-    /*vm.wms = {
-      source: {
-        type: 'ImageWMS',
-        url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
-        params:{'LAYERS': 'uptb_gis_bna:jalan_line_wgs0', 'TILED': true}
+    function test(){
+      console.log(vm.layers[1].active);
+      var i = 0;
+      for (i=0; i<vm.layers.length; i++) {
+        vm.layers[i].active = vm.tampil;
       }
-    };*/
+    };
+    vm.test = test;
+    //test get http data from API 
+    /*$http.get('https://jsonplaceholder.typicode.com/posts').then(function (response){
+      vm.getJson = response.data;
+    });*/
 
-    //render all basic layers land,perumahan,bangunan,laut,tambak,sungai,hutan,taman,lapangan,jalan,lokasi
+
+    //List all basic layers land,perumahan,bangunan,laut,tambak,sungai,hutan,taman,lapangan,jalan,lokasi
     vm.layers = [
       {
         name: 'land',
@@ -145,7 +152,85 @@
         },
         zIndex: 11
       }
-      ];
+    ];
+
+    //List all tematik layers jml_kk_2011, total_pddk_2013, total_pddk_2014, 
+    vm.tematik = [
+      {
+        name: 'jml_kk_2011',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:tematik_jml_kk', 'TILED': true}
+        },
+        zIndex: 12
+      },
+      {
+        name: 'total_pddk_2013',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:total_pddk_2013', 'TILED': true}
+        },
+        zIndex: 12
+      },
+      {
+        name: 'total_pddk_2014',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:total_pddk_2014', 'TILED': true}
+        },
+        zIndex: 12
+      },
+      {
+        name: 'landuse_2011',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:landuse_20110', 'TILED': true}
+        },
+        zIndex: 12
+      }
+    ];
+
+    //List all perencanaan layers pola_ruang,kawasan_strategis,pusat_pelayanan
+    vm.rencana = [
+      {
+        name: 'pola_ruang',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:pola_ruang_banda_aceh_utm', 'TILED': true}
+        },
+        zIndex: 13
+      },
+      {
+        name: 'kawasan_strategis',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:kawasan strategis1', 'TILED': true}
+        },
+        zIndex: 13
+      },
+      {
+        name: 'pusat_pelayanan',
+        active: false,
+        source: {
+            type: 'TileWMS',
+            url: 'http://bappeda.bandaacehkota.go.id/geoserver/uptb_gis_bna/wms',
+            params:{'LAYERS': 'uptb_gis_bna:struktur_ruang', 'TILED': true}
+        },
+        zIndex: 13
+      }
+    ];
 
   }
   
