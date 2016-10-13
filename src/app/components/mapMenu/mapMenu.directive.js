1f4c6222c159df5d7e1658825c9287e74c6af416
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-10-13 11:44:55 +0700

    Move mapMenu accordion to its own component

---
+++ src/app/components/mapMenu/mapMenu.directive.js
@@ -0,0 +1,74 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gulpAngular')
+    .directive('mapMenu', mapMenu);
+
+  /** @ngInject */
+  function mapMenu() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/mapMenu/mapMenu.html',
+      scope: {},
+      controller: MapMenuController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function MapMenuController($scope, $location, lokasiService) {
+      var vm = this;
+
+      //status of each collapsible accordion
+      //TODO: dynamic status.open
+      vm.status = {
+        open: false,
+        open2: false,
+        open3: false,
+        open4: false,
+        open5: false,
+        isCustomHeaderOpen: false,
+        isFirstOpen: true,
+        isFirstDisabled: false
+      };
+
+      //TODO: use one function and reuse it for different toggle layers
+      //toggle all for basic layers
+      vm.basicCheckBox = true;
+      function toggleBasicMap(){
+        var i = 0;
+        for (i=0; i<vm.layers.length; i++) {
+          vm.layers[i].active = vm.basicCheckBox;
+        }
+      }
+
+      vm.toggleBasicMap = toggleBasicMap;
+
+      //toggle all for tematik layers
+      vm.tematikCheckBox = false;
+      function toggleTematikMap(){
+        var i = 0;
+        for (i=0; i<vm.tematik.length; i++) {
+          vm.tematik[i].active = vm.tematikCheckBox;
+        }
+      }
+
+      vm.toggleTematikMap = toggleTematikMap;
+
+      //toggle all for perencanaan layers
+      vm.rencanaCheckBox = false;
+      function toggleRencanaMap(){
+        var i = 0;
+        for (i=0; i<vm.rencana.length; i++) {
+          vm.rencana[i].active = vm.rencanaCheckBox;
+        }
+      }
+      vm.toggleRencanaMap = toggleRencanaMap;
+
+    }
+  }
+
+})();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -46,41 +46,6 @@
     //   {name: sssss, active: true}
     // ];
 
-    //TODO: use one function and reuse it for different toggle layers
-    //toggle all for basic layers
-    vm.basicCheckBox = true;
-    function toggleBasicMap(){
-      var i = 0;
-      for (i=0; i<vm.layers.length; i++) {
-        vm.layers[i].active = vm.basicCheckBox;
-      }
-    }
-
-    vm.toggleBasicMap = toggleBasicMap;
-
-    //toggle all for tematik layers
-    vm.tematikCheckBox = false;
-    function toggleTematikMap(){
-      var i = 0;
-      for (i=0; i<vm.tematik.length; i++) {
-        vm.tematik[i].active = vm.tematikCheckBox;
-      }
-    }
-
-    vm.toggleTematikMap = toggleTematikMap;
-
-    //toggle all for perencanaan layers
-    vm.rencanaCheckBox = false;
-    function toggleRencanaMap(){
-      var i = 0;
-      for (i=0; i<vm.rencana.length; i++) {
-        vm.rencana[i].active = vm.rencanaCheckBox;
-      }
-    }
-    vm.toggleRencanaMap = toggleRencanaMap;
-
-
-
     //test get http data from API
     /*$http.get('https://jsonplaceholder.typicode.com/posts').then(function (response){
       vm.getJson = response.data;
@@ -523,20 +488,6 @@
 
 
     }); //end onClick
-
-
-    //status of each collapsible accordion
-    //TODO: dynamic status.open
-    vm.status = {
-      open: false,
-      open2: false,
-      open3: false,
-      open4: false,
-      open5: false,
-      isCustomHeaderOpen: false,
-      isFirstOpen: true,
-      isFirstDisabled: false
-    };
 
   }
 
