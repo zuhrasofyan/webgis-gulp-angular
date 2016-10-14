5b25fa2a77cb8d1639c52c0899ff3a14be349f1b
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-10-14 12:09:09 +0700

    Move map small buttons group to a component

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -40,65 +40,8 @@
       vm.lokasiMenuIsVisible = vm.lokasiMenuIsVisible ? false : true;
     };
 
-    //inject rotate north button to directive
-    var rotateNorthControl = function(opt_options) {
-        var options = opt_options || {};
-        var rotation = 0;
-        var button = document.createElement('div');
-        button.className = "btn btn-info btn-sm";
-        button.innerHTML = '<i class="fa fa-rotate-right"></i>';
-        var this_ = this;
-        var handleRotateNorth = function(e) {
-            rotation += 90;
-            this_.getMap().getView().setRotation(rotation);
-            // console.log(this_.getMap().getView().getCenter());
-            // console.log(this_.getMap().getLayers());
-            // console.log(options.target);
-        };
-        button.addEventListener('click', handleRotateNorth, false);
-        button.addEventListener('touchstart', handleRotateNorth, false);
-        var element = document.createElement('div');
-        element.className = 'rotate-north ol-unselectable';
-        element.setAttribute('title', 'Rotate 90 degree');
-        element.appendChild(button);
-        ol.control.Control.call(this, {
-            element: element,
-            target: options.target
-        });
-    };
 
-    ol.inherits(rotateNorthControl, ol.control.Control);
 
-    vm.rotateNorth = {
-        control: new rotateNorthControl()
-    };
-
-    function btnZoomIn() {
-      if (vm.bandaAceh.zoom < vm.defaults.view.maxZoom) {
-        vm.bandaAceh.zoom = vm.bandaAceh.zoom+1;
-      }
-
-    }
-    vm.btnZoomIn = btnZoomIn;
-
-    function btnZoomOut() {
-      if (vm.bandaAceh.zoom > vm.defaults.view.minZoom) {
-        vm.bandaAceh.zoom = vm.bandaAceh.zoom-1;
-      }
-    }
-
-    vm.btnZoomOut = btnZoomOut;
-
-    //save map as image
-    function saveAsPNG() {
-      var canvas = document.getElementsByTagName('canvas')[0];
-      //console.log(canvas);
-      canvas.toBlob(function (blob) {
-        //console.log(blob);
-        saveAs(blob, 'map.png');
-      }, 'image/png');
-    }
-    vm.saveAsPNG = saveAsPNG;
 
 
     // vm.controls = [
