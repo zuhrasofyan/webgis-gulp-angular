8873ec211c98e4817d44a72ec87fa3e9524a05bd
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-11-15 13:42:35 +0700

    Cleaning bugs

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,7 +6,7 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($scope, $http, olData, lokasiService, basemapDataService, tematikService, rencanaService, baseDataService) {
+  function MainController($scope, $http, olData, lokasiService, basemapDataService, tematikService, rencanaService, baseDataService, html2canvasAngular) {
     var vm = this;
 
     //set initial data for popup-label
@@ -16,7 +16,6 @@
     vm.bandaAceh = baseDataService.bandaAceh;
     //set default view configuration
     vm.defaults = baseDataService.defaults;
-    vm.mousePosition = {};
     vm.projection = baseDataService.projection;
 
     //test get http data from API
@@ -26,7 +25,6 @@
 
     //button controls on the map
     vm.controls = baseDataService.controls;
-
 
     // vm.controls = [
     //   {name: 'rotateNorth', active:true, btn: vm.rotateNorth},
@@ -44,102 +42,96 @@
     //get other basemap layers from otherBasemapService
     vm.basemaps = basemapDataService.data;
 
-    //add mouse position listener
-    $scope.$on('openlayers.map.pointermove', function(event, data){
-      //console.log(data.coord);
-      if (vm.projection === data.projection) {
-        $scope.$apply(function(){
-          vm.mousePosition = data.coord;
-
-        });
-      } else {
-        vm.mousePosition = '000';
-      }
-
-    });
-
 
     //add event onclick to show detail information for each point
     $scope.$on('openlayers.map.singleclick', function(event, data) {
 
-      //use this type of code to setcenter to a new coordinate and place marker (eg from user input or from searchbox)
-      // var coordi = data.feature.getGeometry().getCoordinates();
-      // olData.getMap().then(function(map){
-      //   var layers3 = map.getLayers();
-      //   console.log(layers3);
-      //   map.setCenter(longi,lati);
-      //   //map.getView().
-      // });
       //get projection data
+      olData.getMap().then(function(map){
+        //prj variable to ensure projection of data.coordinate into EPSG:3857
+        var prj = ol.proj.transform([ data.coord[0], data.coord[1] ], data.projection, 'EPSG:3857').map(function(c) {
+            return c;
+        });
 
-      //use this type of code to get data from each layer based on the name
-      // olData.getMap().then(function(map){
-      //   var layers = map.getLayers();
-      //   layers.forEach(function(layer) {
-      //     if (layer.get('name') === 'landuse_2011') {
-      //       console.log ('haha');
-      //     }
-      //   });
-      // });
+        //anything elses
 
-      var prj = ol.proj.transform([ data.coord[0], data.coord[1] ], data.projection, 'EPSG:3857').map(function(c) {
-          return c;
-      });
+        var layers = map.getLayers();
 
-      var prettyCoord = ol.coordinate.toStringHDMS(prj);
+        var prettyCoord = ol.coordinate.toStringHDMS(prj);
 
-      //get lattitude and longitude coordinate. lat = latLon[1]; lon = latLon[0];
-      var latLon = ol.proj.transform(prj,'EPSG:3857', 'EPSG:4326');
+        //get lattitude and longitude coordinate. lat = latLon[1]; lon = latLon[0];
+        var latLon = ol.proj.transform(prj,'EPSG:3857', 'EPSG:4326');
 
-      //hack view not using angular way to use OL3 getResolution()
-      //initialize manual map view to retrieve zoom level, resolution and then get the  feature info JSON data from geoserver url based on this coordinate
-      var view = new ol.View({
-          center: [0, 0],
-          zoom: vm.bandaAceh.zoom
-      });
-      var viewResolution = /** @type {number} */ (view.getResolution());
+        //hack view not using angular way to use OL3 getResolution()
+        //initialize manual map view to retrieve zoom level, resolution and then get the  feature info JSON data from geoserver url based on this coordinate
+        var view = new ol.View({
+            center: [0, 0],
+            zoom: vm.bandaAceh.zoom
+        });
+        var viewResolution = /** @type {number} */ (view.getResolution());
 
-      //hack location not using angular way to use OL3 getGetFeatureInfoUrl()
-      //we can add array of active layers to list all getFeatureInfoUrl (but consider the data load when create new ol.source.TileWMS. so if possible find solution for this)
-      var wmsLokasi = new ol.source.TileWMS(vm.lokasi[0].source);
-      var myurl = wmsLokasi.getGetFeatureInfoUrl(
-              prj, viewResolution, 'EPSG:3857',
-              {'INFO_FORMAT': 'application/json'});
+        /* Mencoba mengambil data untuk layer paling akhir dari array map layer*/
+        //get the active layer length
+        var arrleng = layers.getArray().length;
+        if (arrleng > 1) {
+          //get the last added layer from the layers list
+          var aa = layers.getArray()[arrleng-2];
 
-      vm.url=myurl;
-
-      $http.get(myurl).success(
-        function (data, status) {
-          var items = [];
-
-          //if there is data.features returned from geoserver then
-          if (data.features[0]) {
-
-            //show point data lengkap
-            var properties = data.features[0].properties;
-            var nama = properties.nama_lokas;
-            var desa = properties.desa;
-            vm.lengkap =  nama + '  ' + desa + '<br>' + prj;
-
-            //place the popup label on the map
-            vm.isiLabel = {
-                lat: latLon[1],
-                lon: latLon[0],
-                label: {
-                  message: vm.lengkap,
-                  show: true,
-                  showOnMouseClick: true,
-                  showOnMouseHover: true
-                }
-            };
-
-          } else {
-            vm.lengkap = 'no data';
-            vm.isiLabel = {};
-          }
-
+          console.log(aa.get('name') + ' adalah layer paling atas dari : ' + (arrleng-1) + 'layer');
         }
-      );
+
+
+        /*Tampilkan data yang diambil dari spesifik layer (layer 'lokasi')*/
+        //use this type of code to get data from each layer based on the name
+        layers.forEach(function(layer) {
+          if (layer.get('name') === 'lokasi') {
+
+            //hack location not using angular way to use OL3 getGetFeatureInfoUrl()
+            //we can add array of active layers to list all getFeatureInfoUrl (but consider the data load when create new ol.source.TileWMS. so if possible find solution for this)
+            var wmsLokasi = new ol.source.TileWMS(vm.lokasi[0].source);
+            var myurl = wmsLokasi.getGetFeatureInfoUrl(
+                    prj, viewResolution, 'EPSG:3857',
+                    {'INFO_FORMAT': 'application/json'});
+
+            vm.url=myurl;
+
+            $http.get(myurl).success(
+              function (data, status) {
+                var items = [];
+
+                //if there is data.features returned from geoserver then
+                if (data.features[0]) {
+
+                  //show point data lengkap
+                  var properties = data.features[0].properties;
+                  var nama = properties.nama_lokas;
+                  var desa = properties.desa;
+                  vm.lengkap =  nama + '  ' + desa + '<br>' + prj;
+
+                  //place the popup label on the map
+                  vm.isiLabel = {
+                      lat: latLon[1],
+                      lon: latLon[0],
+                      label: {
+                        message: vm.lengkap,
+                        show: true,
+                        showOnMouseClick: true,
+                        showOnMouseHover: true
+                      }
+                  };
+
+                } else {
+                  vm.lengkap = 'no data';
+                  vm.isiLabel = {};
+                }
+
+              }
+            ); //end http get
+          } 
+        });
+      });
+
+
 
     }); //end onClick
 
