04c41c35c92d3153d8f871405b888d1b11d6da6e
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-12-01 12:19:08 +0700

    use http.get cache to prevent re-request

--- src/app/components/mapMenu/mapMenu.directive.js
+++ src/app/components/mapMenu/mapMenu.directive.js
@@ -123,7 +123,6 @@
         }
       }
 
-
       //show/hidden accordions interactions
       vm.layerMenuIsVisible = false;
       vm.showMapMenu = function () {
@@ -148,7 +147,7 @@
         vm.cariMenuIsVisible = vm.cariMenuIsVisible ? false: true;
         // only request and load api_lokasi to vm.lokasiList array after button showMapCariMenu for the first time clicked
         if (vm.lokasiList.length === 0) {
-          $http.get('http://bappeda.bandaacehkota.go.id/webgis/api/api_lokasi.php').then(function (response){
+          $http.get('http://bappeda.bandaacehkota.go.id/webgis/api/api_lokasi.php', {cache: true}).then(function (response){
             vm.lokasiList = response.data;
             //convert array of array response.data as array of object (not needed anymore since already processed into array of object in API)
             //vm.bankList = [];
